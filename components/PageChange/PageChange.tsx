bf4276535d9a47d5eb36f74a6cbc478b356b523b
Tanmay <tanmay.jain@particle41.com>
2022-08-26 14:55:42 +0530

    Tailwind working correctly now.

---
+++ components/PageChange/PageChange.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// reactstrap components
+// import { Spinner } from "reactstrap";
+
+// core components
+
+// export default function PageChange(props: any) {
+//   return <div> Hi </div>;
+// }
+
+const PageChange = (props: any) => {
+  return <div> Hi </div>;
+};
+
+export default PageChange;
